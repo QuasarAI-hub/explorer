d98589f2e484392294eefdd500d2ae41e4ff7a48
Yakovishin Evgeny <00705@inbox.ru>
2024-12-26 22:14:32 +0300

    Fix footer

--- src/layouts/components/NavFooter.vue
+++ src/layouts/components/NavFooter.vue
@@ -6,9 +6,9 @@
     <div class="flex flex-1">
       &copy;&nbsp;
       {{ new Date().getFullYear() }}&nbsp;
-      Made With&nbsp;
-      <img src="../../assets/images/heart.svg" />&nbsp;
-      By&nbsp;
+      Forked with&nbsp;
+      <img src="../../assets/images/heart.svg" alt="love"/>
+      from&nbsp
       <a
         class="link link-primary no-underline"
         href="https://ping.pub"
@@ -16,22 +16,32 @@
         rel="noopener noreferrer"
         >Ping.pub</a
       >
+      &nbspby&nbsp
+      <a
+      class="link link-primary no-underline"
+      href="https://quasarstaking.ai"
+      target="_blank"
+      rel="noopener noreferrer"
+    >Quasar</a
+    >
     </div>
     <div
       class="hidden md:!block"
     >
       <a
         class="link link-primary no-underline mr-4"
-        href="https://github.com/ping-pub/explorer/blob/master/LICENSE"
+        href="https://github.com/QuasarAI-hub/explorer/blob/master/LICENSE"
         target="noopener noreferrer"
         >License</a
       >
       <a
         class="link link-primary no-underline"
-        href="https://github.com/ping-pub/explorer"
+        href="https://github.com/QuasarAI-hub/explorer"
         target="noopener noreferrer"
         >Github</a
       >
     </div>
   </footer>
 </template>
+<script setup lang="ts">
+</script>
